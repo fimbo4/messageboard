c931af396889402186ec015a129d03a4f01f458b
fimbo4 <99178906+fimbo4@users.noreply.github.com>
2024-12-03 02:01:47 +0100

    Add files via upload

--- backend/microservice1/ms1.py
+++ backend/microservice1/ms1.py
@@ -7,7 +7,8 @@ def post_database():
     print("lololololololololololololol", flush=True)
     data = request.json
     print(data.get("identity"), flush=True)
-    response = requests.post("http://api:4001/add_message", json = data)
-    return ""
+    response = requests.post("http://localhost:4001/add_message", json = data)
+    return "hi"
 
-app.run(host='0.0.0.0', port=9001)
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=9001)
--- backend/microservice2/ms2.py
+++ backend/microservice2/ms2.py
@@ -1,4 +1,5 @@
 import requests
+import json
 from flask import Flask, request
 
 app = Flask(__name__)
@@ -7,10 +8,10 @@ app = Flask(__name__)
 def rertrieve_data():
     search_query = request.json
     print(search_query, flush=True)
-    response = requests.post("http://127.0.0.1:4000/get_message", json = search_query)
+    response = requests.post("http://localhost:4001/get_message", json = search_query)
     print("ms2 responmse", flush=True)
-    print(response.text, flush=True)
-    send_response = requests.post("http://127.0.0.1:5001/recieve_message", json = response.text)
+    #print(response.json, flush=True)
+    send_response = requests.post("http://localhost:5001/recieve_message", json = response.json())
     return ""
 
 app.run(host='0.0.0.0', port=9002)
